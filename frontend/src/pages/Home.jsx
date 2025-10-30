03dc28fe489d42c1cf6c4d8a737c78199ab60e7e
NattapatSpu <nattapat.mar@spumail.net>
2025-10-30 20:25:43 +0700

    ล่าสัตว์

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,52 +1,9 @@
-
-// import { useEffect, useState } from 'react';
-// import api from '../api';
-// import CategorySection from '../components/CategorySection';
-// import '../styles/home.css';   // ใช้สไตล์ที่อยู่ในโฟลเดอร์ styles
-
-// const CATS = ['การเงินการลงทุน','มังงะ','นิยาย','อาหารเเละสุขภาพ','การเรียน'];
-
-// export default function Home(){
-//   const [data, setData] = useState({});
-
-//   useEffect(()=>{
-//     (async ()=>{
-//       const result = {};
-//       for (const c of CATS){
-//         const res = await api.get('/books', { params: { category: c, limit: 5 }});
-//         result[c] = res.data;
-//       }
-//       setData(result);
-//     })();
-//   }, []);
-
-//   return (
-//     <main className="home-page">
-//       <section className="hero">
-//         <div className="container">
-//           {/* กล่องไฮไลท์ครอบ h1+p */}
-//           <div className="hero-highlight">
-//             <h1>ยินดีต้อนรับสู่ May i Booking</h1>
-//             <p className="hero-sub">เลือกอ่านได้ตามใจชอบ — หนังสือใหม่อัปเดตตลอด</p>
-//           </div>
-//         </div>
-//       </section>
-
-//       <div className="home-content container">
-//         {CATS.map(c => (
-//           <CategorySection key={c} title={c} items={data[c] || []} />
-//         ))}
-//       </div>
-//     </main>
-//   );
-// }
-
 import { useEffect, useState } from 'react';
 import api from '../api';
 import CategorySection from '../components/CategorySection';
 import '../styles/home.css';   // ใช้สไตล์จากโฟลเดอร์ styles
 
-const CATS = ['การเงินการลงทุน','มังงะ','นิยาย','อาหารเเละสุขภาพ','การเรียน'];
+const CATS = ['การเรียน','มังงะ','การเงินการลงทุน','นิยาย','อาหารเเละสุขภาพ',];
 
 export default function Home() {
   const [data, setData] = useState({});
